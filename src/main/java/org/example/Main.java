ff6ae11a3c70c8060e664d5e48568ea17f85802e
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-06-10 15:27:18 +0200

    build students, teacher and Course in the main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Der NoArgs und AllArgs Konstruktor:
+        Student student1 = new Student();
+        Student student2 = new Student("123","Volker","Büchsenweg",2.4);
+        Map<String,Student> allStudents= new HashMap<>();
+        allStudents.put("123",student1);
+        allStudents.put("456",student2);
+
+        Teacher teacher1 = new Teacher("2","Müller","English");
+
+        Course course1 = new Course("4", "English",teacher1, allStudents );
+
+        //Ausprobieren von Settern und Gettern und toString
+        System.out.println(student1);
+        student1.setId("343");
+        student1.setName("Georg");
+        student1.setAdress("Fahrenwinkel");
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student1.getAdress()+"\n");
+
+        //Testen der Equals Methode
+        System.out.println("Sind Student1 und Student2 identisch? " + student1.equals(student2)+"\n");
+
+        //toString von Course
+        System.out.println(course1);
+
+
     }
 }
