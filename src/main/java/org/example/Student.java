de0274d61fdde7b8f757607078887b89054188e6
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-06-10 15:00:25 +0200

    add Student class and Teacher Record

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Student {
+    private String id;
+    private String name;
+    private String adress;
+    private double score;
+
+}
